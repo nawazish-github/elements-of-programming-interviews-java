c83296a8f69ed2a0e366300f5ff66e83e5f90bf7
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2019-01-02 20:37:07 +0530

    Create a test utility class which would init LinkedList

--- src/test/java/TestListUtil.java
+++ src/test/java/TestListUtil.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import list.util.ListNode;
 
 public class TestListUtil {
+    public static ListNode<Integer> setupLinkedList(int...nums) {
+        ListNode<Integer> head = new ListNode<Integer>();
+        ListNode<Integer> tail = head;
+
+        for (int i=0;i<nums.length; i++){
+            ListNode l = new ListNode<Integer>();
+            l.data=nums[i];
+            tail.next=l;
+            tail = l;
+        }
+        return head.next;
+    }
 }
