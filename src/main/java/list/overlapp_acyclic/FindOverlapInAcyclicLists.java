ab4f17418d2d4ceeee5d468eb39ec13f0461b25e
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2019-02-08 01:03:38 +0530

    Add test and implementation to findout the overlapping node

--- src/main/java/list/overlapp_acyclic/FindOverlapInAcyclicLists.java
+++ src/main/java/list/overlapp_acyclic/FindOverlapInAcyclicLists.java
@@ -21,4 +21,40 @@ public class FindOverlapInAcyclicLists {
         }
         return false;
     }
+
+    public ListNode<Integer> getOverlappingNode(ListNode<Integer> list1, ListNode<Integer> list2) {
+        int lenList1 = FindOverlapInAcyclicLists.getLength(list1);
+        int lenList2 = FindOverlapInAcyclicLists.getLength(list2);
+        int diff = 0, c = 0;
+
+        if(lenList1 > lenList2){
+            diff = lenList1 - lenList2;
+            while(c++ < diff){
+                list1 = list1.next;
+            }
+        }else{
+            diff = lenList2 - lenList1;
+            while(c++ <= diff){
+                list2 = list2.next;
+            }
+        }
+        while(list1!=null && list2 != null){
+            if (list1.next.data == list2.next.data){
+                return list1.next;
+            }
+            list1 = list1.next;
+            list2 = list2.next;
+        }
+
+        return null;
+    }
+
+    private static int getLength(ListNode<Integer> list) {
+        int counter = 0;
+        while(list!=null){
+            list = list.next;
+            ++counter;
+        }
+        return counter;
+    }
 }
--- src/test/java/TestOverlapinAcyclicLists.java
+++ src/test/java/TestOverlapinAcyclicLists.java
@@ -7,13 +7,22 @@ public class TestOverlapinAcyclicLists {
 
     @Test
     public void testDetectOverlapAndTheOverlappingNode(){
+        FindOverlapInAcyclicLists findOverlapInAcyclicLists = new FindOverlapInAcyclicLists();
+
         ListNode<Integer> list1 = TestListUtil.setupLinkedList(1,2,3,4,5);
         ListNode<Integer> list2 = TestListUtil.setupLinkedList(8,3,4,5);
-
-        FindOverlapInAcyclicLists findOverlapInAcyclicLists = new FindOverlapInAcyclicLists();
         boolean isOverlapping = findOverlapInAcyclicLists.detectOverlap(list1, list2);
-
         Assertions.assertEquals(true, isOverlapping);
 
+        list1 = TestListUtil.setupLinkedList(1,2,3,4,5);
+        list2 = TestListUtil.setupLinkedList(8,3,4,5);
+        ListNode<Integer>overLappingNode = findOverlapInAcyclicLists.getOverlappingNode(list1,list2);
+        Assertions.assertEquals(3, overLappingNode.data);
+
+        list1 = TestListUtil.setupLinkedList(1,2,3,4,5);
+        list2 = TestListUtil.setupLinkedList(8,9,10);
+        isOverlapping = findOverlapInAcyclicLists.detectOverlap(list1, list2);
+        Assertions.assertEquals(false, isOverlapping);
     }
+
 }
