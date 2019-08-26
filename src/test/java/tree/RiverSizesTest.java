0eb95c5426888773e8681d7cb20adaf8e0cdea2d
Mohammad-Nawazish-Khan <Mohammad_Nawazish_Khan@external.mckinsey.com>
2019-08-26 12:21:21 +0530

    Add test to RiverSizes problem

--- src/test/java/tree/RiverSizesTest.java
+++ src/test/java/tree/RiverSizesTest.java
@@ -1,13 +1,28 @@
 package tree;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class RiverSizes {
+public class RiverSizesTest {
 
     @Test
     public void riverSizesTest(){
-
+        int [][] inputMatrix = new int[][]{
+                {1,0,0,1,0},
+                {1,0,1,0,0},
+                {0,0,1,0,1},
+                {1,0,1,0,1},
+                {1,0,1,1,0}
+        };
+       RiverSizes riverSizes = new RiverSizes();
+       ArrayList<Integer> integers = riverSizes.riverSizes(inputMatrix);
+        Assertions.assertEquals(5, integers.size());
+        Integer[] i = new Integer[5];
+        Integer[] j = integers.toArray(i);
+        Arrays.sort(j);
+        Assertions.assertArrayEquals(new Integer[]{1,2,2,2,5}, j);
     }
 }
