5420115dadccb74b43752090aaf2933a1f866536
Mohammad-Nawazish-Khan <Mohammad_Nawazish_Khan@external.mckinsey.com>
2019-09-08 14:29:11 +0530

    Solve remove Kth Node from end and permutation problem

--- src/test/java/RemoveKthNodeFromEndTest.java
+++ src/test/java/RemoveKthNodeFromEndTest.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import list.removal.RemoveKthNodeFromEnd;
+import list.util.ListNode;
+import org.junit.jupiter.api.Test;
 
 public class RemoveKthNodeFromEndTest {
+
+    @Test
+    public void removeKthNodeFromEndTest(){
+        ListNode<Integer> head = TestListUtil.setupLinkedList(1, 2, 3, 4, 5);
+        ListNode<Integer> expected = TestListUtil.setupLinkedList(1, 2, 4, 5);
+        RemoveKthNodeFromEnd removeKthNodeFromEnd = new RemoveKthNodeFromEnd();
+        removeKthNodeFromEnd.removeKthNodeFromEnd(head, 3);
+        TestListUtil.assertLinkedLists(head, expected);
+    }
+
+    @Test
+    public void removeKthNodeFromEndTest2(){
+        ListNode<Integer> head = TestListUtil.setupLinkedList(1, 2, 3, 4, 5);
+        ListNode<Integer> expected = TestListUtil.setupLinkedList(1, 2, 3, 4);
+        RemoveKthNodeFromEnd removeKthNodeFromEnd = new RemoveKthNodeFromEnd();
+        removeKthNodeFromEnd.removeKthNodeFromEnd(head, 1);
+        TestListUtil.assertLinkedLists(head, expected);
+    }
+
+
+    @Test
+    public void removeKthNodeFromEndTest3(){
+        ListNode<Integer> head = TestListUtil.setupLinkedList(1, 2, 3, 4, 5);
+        ListNode<Integer> expected = TestListUtil.setupLinkedList(2, 3, 4, 5);
+        RemoveKthNodeFromEnd removeKthNodeFromEnd = new RemoveKthNodeFromEnd();
+        removeKthNodeFromEnd.removeKthNodeFromEnd(head, 5);
+        TestListUtil.assertLinkedLists(head, expected);
+    }
 }
--- src/test/java/recursion/PermutationTest.java
+++ src/test/java/recursion/PermutationTest.java
@@ -1,4 +1,26 @@
 package recursion;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 public class PermutationTest {
+
+    @Test
+    public void testPermutation(){
+        Permutation permutation = new Permutation();
+        permutation.getPermutations(Arrays.asList(1,2,3));
+    }
+
+    @Test
+    public void testPermutation2(){
+        Permutation permutation = new Permutation();
+        permutation.getPermutations(Arrays.asList(1));
+    }
+
+    @Test
+    public void testPermutation3(){
+        Permutation permutation = new Permutation();
+        permutation.getPermutations(Arrays.asList());
+    }
 }
