08430c1bec08cfbd46e874d70961eeea098b2862
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2019-12-31 23:53:17 +0530

    Solve umbrella problem

---
+++ src/main/java/miscinterviews/UmbrellaProblem.java
@@ -0,0 +1,50 @@
+package miscinterviews;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Hackerrank problem.
+ * Given number of people and an array of sizes of Umbrella.
+ * Find all combinations of umbrellas that would fit the peoples exactly.
+ * Also find the minimum, umbrella which could be used to exactly fit all people.
+ */
+public class UmbrellaProblem {
+
+    public static void main(String[] args) {
+        umbrella(6, new int[]{3, 6});
+    }
+
+    public static void umbrella (int people, int [] arr){
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        umbrella(people, arr, new ArrayList<Integer>(), result);
+        System.out.println(result);
+
+        Collections.sort(result, (List<Integer>l1, List<Integer> l2) ->{
+            if(l1.size() < l2.size()) return -1;
+            if(l1.size() > l2.size()) return 1;
+            return 0;
+        });
+
+        System.out.println(result.get(0).size());
+    }
+
+    private static void umbrella ( int people, int[] arr, List<Integer> partial, List<List<Integer>> result){
+        if(people < 0){
+            partial.clear();
+            return;
+        }
+
+        if(people == 0){
+            result.add(new ArrayList<Integer>(partial));
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            partial.add(arr[i]);
+            umbrella(people-arr[i], arr, partial, result);
+            partial.clear();
+        }
+    }
+}
