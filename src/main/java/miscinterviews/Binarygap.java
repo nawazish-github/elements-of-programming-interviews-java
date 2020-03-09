4e26b53ed79bce0aa4f73be311e1e3d4f2b1e2cf
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2020-03-09 11:31:35 +0530

    Add Binarygap prompt

--- src/main/java/miscinterviews/Binarygap.java
+++ src/main/java/miscinterviews/Binarygap.java
@@ -1,5 +1,18 @@
 package miscinterviews;
 
+/**
+ * Given a positive integer N, find and return the longest distance between two consecutive 1's in the binary representation of N.
+ * If there aren't two consecutive 1's, return 0.
+ *
+ * Input: 22
+ * Output: 2
+ * Explanation:
+ * 22 in binary is 0b10110.
+ * In the binary representation of 22, there are three ones, and two consecutive pairs of 1's.
+ * The first consecutive pair of 1's have distance 2.
+ * The second consecutive pair of 1's have distance 1.
+ * The answer is the largest of these two distances, which is 2.
+ */
 class Binarygap {
     public int solution(int N) {
         // write your code in Java SE 8
