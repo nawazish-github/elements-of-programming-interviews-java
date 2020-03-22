1560ad37dbcc3c367d00dda5224ad5ac00d1f801
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2020-03-22 21:16:17 +0530

    Add restricted combination solution with bottom up approach

--- src/main/java/recursion/combination/returnvalue/RestrictedCombination.java
+++ src/main/java/recursion/combination/returnvalue/RestrictedCombination.java
@@ -1,4 +1,44 @@
 package recursion.combination.returnvalue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RestrictedCombination {
+
+    public List<List<Integer>> combinate(List<Integer> arr, int restriction){
+        return combinate(arr, 0, restriction, 0);
+    }
+
+    private List<List<Integer>> combinate(List<Integer> arr, int i, int restriction, int currSize) {
+       if(i == arr.size() && currSize != restriction){
+           return new ArrayList<>();
+       }
+       if(restriction > arr.size()){
+           return new ArrayList<>();
+       }
+
+       if(i == arr.size()){
+           List<List<Integer>> result = new ArrayList<>();
+           result.add(new ArrayList<>());
+           return result;
+       }
+
+       Integer currNum = arr.get(i);
+       List<List<Integer>> returnable = new ArrayList<>();
+       List<List<Integer>> combinateIncl = combinate(arr, i + 1, restriction, currSize + 1);
+       List<List<Integer>> combinateExcl = combinate(arr, i + 1, restriction, currSize);
+
+       for(List<Integer> incl : combinateIncl){
+           incl.add(currNum);
+       }
+       returnable.addAll(combinateIncl);
+       returnable.addAll(combinateExcl);
+       return returnable;
+    }
+
+    public static void main(String[] args) {
+        RestrictedCombination restrictedCombination = new RestrictedCombination();
+        List<List<Integer>> combinate = restrictedCombination.combinate(List.of(1, 2, 3), 2);
+        System.out.println(combinate);
+    }
 }
