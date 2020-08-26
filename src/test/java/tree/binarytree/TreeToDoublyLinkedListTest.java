01a8421fa47c3bfb84939e9d22dbd38622f9d25a
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2020-08-27 01:22:52 +0530

    Ignore tests final

--- src/test/java/tree/binarytree/TreeToDoublyLinkedListTest.java
+++ src/test/java/tree/binarytree/TreeToDoublyLinkedListTest.java
@@ -3,17 +3,16 @@ package tree.binarytree;
 import list.util.BinaryNode;
 import org.junit.Ignore;
 
-@Ignore
 public class TreeToDoublyLinkedListTest {
 
     //@Test
     //@Ignore
     public void test1(){
-        BinaryNode<Integer> two = new BinaryNode<>(2, null, null);
+        /*BinaryNode<Integer> two = new BinaryNode<>(2, null, null);
         BinaryNode<Integer> three = new BinaryNode<>(3, null, null);
         BinaryNode<Integer> one = new BinaryNode<>(1, two, three);
         TreeToDoublyLinkedList treeToDoublyLinkedList = new TreeToDoublyLinkedList();
         BinaryNode<Integer> dll = treeToDoublyLinkedList.transform(one);
-        System.out.println(dll);
+        System.out.println(dll);*/
     }
 }
