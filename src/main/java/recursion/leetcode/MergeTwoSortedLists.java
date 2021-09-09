04deb91ea7cfc0ff42a8db61be6d1723f24da3a1
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2021-09-09 21:31:24 +0530

    Solve leetcode merge two sorted problem

--- src/main/java/recursion/leetcode/MergeTwoSortedLists.java
+++ src/main/java/recursion/leetcode/MergeTwoSortedLists.java
@@ -3,13 +3,13 @@ package recursion.leetcode;
 import list.util.ListNode;
 
 public class MergeTwoSortedLists {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode toReturn = new ListNode();
+    public ListNode<Integer> mergeTwoLists(ListNode<Integer> l1, ListNode<Integer> l2) {
+        ListNode<Integer> toReturn = new ListNode<Integer>();
         mergeTwoLists(l1, l2, toReturn, toReturn);
         return toReturn.next;
     }
 
-    public void mergeTwoLists(ListNode l1, ListNode l2, ListNode toReturn, ListNode addHere) {
+    public void mergeTwoLists(ListNode<Integer> l1, ListNode<Integer> l2, ListNode<Integer> toReturn, ListNode<Integer> addHere) {
 
         if (l1 == null && l2 == null) return;
         if (l1 == null ^ l2 == null) {
@@ -22,15 +22,15 @@ public class MergeTwoSortedLists {
             }
         }
 
-        if (l1.val == l2.val) {
-            addHere.next = new ListNode(l1.val);
-            addHere.next.next = new ListNode(l1.val);
+        if (l1.data == l2.data) {
+            addHere.next = new ListNode<Integer>(l1.data);
+            addHere.next.next = new ListNode<Integer>(l1.data);
             mergeTwoLists(l1.next, l2.next, toReturn, addHere.next.next);
-        } else if (l1.val < l2.val) {
-            addHere.next = new ListNode(l1.val);
+        } else if (l1.data < l2.data) {
+            addHere.next = new ListNode<Integer>(l1.data);
             mergeTwoLists(l1.next, l2, toReturn, addHere.next);
-        } else if (l2.val < l1.val) {
-            addHere.next = new ListNode(l2.val);
+        } else if (l2.data < l1.data) {
+            addHere.next = new ListNode<Integer>(l2.data);
             mergeTwoLists(l1, l2.next, toReturn, addHere.next);
         }
     }
